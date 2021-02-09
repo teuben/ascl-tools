130fe59cb378e7f37e522d82e11e92a890d5509c
Peter Teuben <teuben@gmail.com>
2021-02-09 16:30:43 -0500

    drafted first version

---
+++ ads/ads1.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+#
+#  https://github.com/adsabs/ads-examples
+#  https://ads.readthedocs.io/en/latest/
+#  https://github.com/adsabs/adsabs-dev-api     curl
+#  Needs: ~/.ads/dev_key
+#
+
+import os
+import sys
+
+#  - sandbox doesn't seem to work (jan 2021)
+#import ads.sandbox as ads
+import ads
+
+
+#  listing with code names
+arg1 = sys.argv[1]
+
+codes = open(arg1).readlines()
+for code in codes:
+    code = code.strip()
+    if code[0] == '#':
+        continue
+
+    print("# CODE:",code)
+
+    # lazy loading (expensive)
+    #q = ads.SearchQuery(full=code, sort=year)
+
+    # loading with fields ahead of time
+    q = ads.SearchQuery(full=code, fl=['title','first_author','year','citation_count','bibcode'],sort='year', rows=10)
+
+    n1 = 0
+    for paper in q:
+        print(paper.year, paper.citation_count, paper.bibcode, paper.first_author, paper.title)
+        n1 = n1 + 1
+
+    q1=q.response.get_ratelimits()
+    print('# %d %s\n' % (n1,q1['remaining']))
